96296458805e02cf3ac204e768974c264f8ba7dc
vishal mehta <professionalprovishal@gmail.com>
2025-05-31 14:31:47 +0530

    isEdgefn append

--- content.js
+++ content.js
@@ -14,6 +14,23 @@ const BLOCKED_KEYWORDS = [
 	"verwaltung", // German for "management"
 ];
 
+// Define isEdge function directly since we're in MAIN world
+window.isEdge = function () {
+	return true;
+};
+
+// Make it non-configurable and non-writable
+try {
+	Object.defineProperty(window, "isEdge", {
+		value: window.isEdge,
+		configurable: false,
+		writable: false,
+		enumerable: true,
+	});
+} catch (e) {
+	console.log("Failed to make isEdge non-writable:", e);
+}
+
 // Block page immediately before content loads
 function blockPage() {
 	document.documentElement.innerHTML = `
